0c2e35f84237c00babab016c0e6ceac1fab1c048
willxxy <willhan327@outlook.com>
2025-01-10 15:14:12 +0000

    limit num_threads

--- ecg_bench/preprocess_ecg.py
+++ ecg_bench/preprocess_ecg.py
@@ -1,5 +1,9 @@
 import argparse
 import os
+os.environ['OPENBLAS_NUM_THREADS'] = '4'
+os.environ['MKL_NUM_THREADS'] = '4'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '4'
+os.environ['NUMEXPR_NUM_THREADS'] = '4'
 
 from ecg_bench.utils.dir_file_utils import FileManager  # Updated import path
 from ecg_bench.utils.preprocess_utils import PreprocessECG
@@ -9,7 +13,7 @@ def get_args():
     parser.add_argument('--data', type = str, default = None, help = 'Please choose the dataset')
     parser.add_argument('--seg_len', type = int, default = 500, help = 'Please choose the segment length')
     parser.add_argument('--target_sf', type = int, default = 250, help = 'Please choose the target sampling frequency')
-    parser.add_argument('--num_cores', type = int, default = 6, help = 'Please choose the number of cores, usually 4-6 is enough')
+    parser.add_argument('--num_cores', type = int, default = 4, help = 'Please choose the number of cores, usually 4-6 is enough')
     parser.add_argument('--num_percentiles', type = int, default = 300000, help = 'Please choose the number of samples for calculating percentiles')
     parser.add_argument('--num_tok_samples', type = int, default = 300000, help = 'Please choose the number of samples for training the tokenizer')
     parser.add_argument('--max_clusters', type = int, default = 200, help = 'Please choose the maximum number of clusters to consider during sampling for training tokenizer')
