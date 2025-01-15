fb089f85643f7ea87830c9354304620dff614852
willxxy <willhan327@outlook.com>
2025-01-15 21:42:12 +0000

    some more progress on dataloader

--- ecg_bench/scripts/train_encoder.sh
+++ ecg_bench/scripts/train_encoder.sh
@@ -2,39 +2,43 @@
 
 echo "ecg_instruct_45k_mapped"
 
+# python train_encoder.py \
+# --data=ecg_instruct_45k_mapped \
+# --model=clip \
+# --gpus=4,5 \
+# --dis \
+# --dev
+
+# echo "----------------------------------------"
+
 python train_encoder.py \
 --data=ecg_instruct_45k_mapped \
 --model=clip \
---gpus=4,5 \
---dis \
+--device=cuda:0 \
 --dev
 
 echo "----------------------------------------"
 
+echo "pretrain_mimic_mapped"
+
+# python train_encoder.py \
+# --data=pretrain_mimic_mapped \
+# --model=clip \
+# --gpus=4,5 \
+# --dis \
+# --dev
+
+# echo "----------------------------------------"
+
 python train_encoder.py \
---data=ecg_instruct_45k_mapped \
+--data=pretrain_mimic_mapped \
 --model=clip \
---device=cuda:4 \
+--device=cuda:0 \
 --dev
 
-# echo "----------------------------------------"
+echo "----------------------------------------"
 
-# echo "pretrain_mimic_mapped"
-
-# python train_encoder.py \
-# --data=pretrain_mimic_mapped \
-# --gpus=4,5 \
-# --dis
-
-# echo "----------------------------------------"
-
-# python train_encoder.py \
-# --data=pretrain_mimic_mapped \
-# --device=cuda:4
-
-# echo "----------------------------------------"
-
-# echo "ecg-qa_mimic-iv-ecg_mapped"
+echo "ecg-qa_mimic-iv-ecg_mapped"
 
 # python train_encoder.py \
 # --data=ecg-qa_mimic-iv-ecg_mapped \
@@ -43,13 +47,15 @@ python train_encoder.py \
 
 # echo "----------------------------------------"
 
-# python train_encoder.py \
-# --data=ecg-qa_mimic-iv-ecg_mapped \
-# --device=cuda:4
+python train_encoder.py \
+--data=ecg-qa_mimic-iv-ecg_mapped \
+--model=clip \
+--device=cuda:0 \
+--dev
 
-# echo "----------------------------------------"
+echo "----------------------------------------"
 
-# echo "ecg-qa_ptbxl_mapped"
+echo "ecg-qa_ptbxl_mapped"
 
 # python train_encoder.py \
 # --data=ecg-qa_ptbxl_mapped \
@@ -58,7 +64,9 @@ python train_encoder.py \
 
 # echo "----------------------------------------"
 
-# python train_encoder.py \
-# --data=ecg-qa_ptbxl_mapped \
-# --device=cuda:4
+python train_encoder.py \
+--data=ecg-qa_ptbxl_mapped \
+--device=cuda:0 \
+--model=clip \
+--dev
 
--- ecg_bench/utils/data_loader_utils.py
+++ ecg_bench/utils/data_loader_utils.py
@@ -11,19 +11,45 @@ class ECGDataset(Dataset):
         self.fm = fm
         self.args = args
         
+        # For datasets that don't have question
+        self.uniform_question = 'Could you please help me explain my ECG?'
+        
     def __len__(self):
         return len(self.json_data_file)
 
     def __getitem__(self, idx):
         instance = self.json_data_file[idx]
-        ecg_path = instance['ecg_path']
-        ecg_signal = self.fm.open_ecg(ecg_path)
-        text = instance['text']
+        self.prepare_instance(instance)
         
         return_dic = {
-            'ecg_path': ecg_path,
-            'text': text
+
         }
         
         return return_dic
+        
+    def prepare_instance(self, instance):
+        ### from json file
+        altered_text = instance['text']
+        if self.args.data == 'pretrain_mimic_mapped':
+            question, answer = altered_text[0]['value'].replace('\n', '').replace('<ecg>', ''), altered_text[1]['value']
+        elif self.args.data in ['ecg-qa_mimic-iv-ecg_mapped', 'ecg-qa_ptbxl_mapped']:
+            question_type, question, answer = altered_text[0], altered_text[1], altered_text[2]
+            answer = ' '.join(answer) if isinstance(answer, list) else answer
+        print(question)
+        print(answer)
+        
+        ### from numpy file
+        np_path = instance['ecg_path']
+        ecg_path = self.fm.open_npy(np_path)
+        ecg_signal = ecg_path['ecg']
+        original_report = ecg_path['report']
+        print(ecg_signal.shape)
+        print(original_report)
+        
+        return_dic = {
+            'question': question,
+            'answer': answer,
+            'ecg': ecg_signal,
+            'original_report': original_report
+        }
         
