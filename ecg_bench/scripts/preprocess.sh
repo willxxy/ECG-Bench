2440d3374513e0ee751c77a0f74e79900d2ecce6
willxxy <willhan327@outlook.com>
2025-03-17 18:10:40 +0000

    get rid of mlx require

--- ecg_bench/scripts/preprocess.sh
+++ ecg_bench/scripts/preprocess.sh
@@ -2,7 +2,8 @@
 
 ### BASE DATA
 # Define an array of base_data values to process
-BASE_DATA_VALUES=("ptb" "mimic" "code15" "cpsc" "csn")
+# BASE_DATA_VALUES=("ptb" "mimic" "code15" "cpsc" "csn")
+BASE_DATA_VALUES=("code15")
 
 # Loop through each base_data value
 for base_data in "${BASE_DATA_VALUES[@]}"; do
@@ -10,10 +11,10 @@ for base_data in "${BASE_DATA_VALUES[@]}"; do
     python preprocess_ecg.py --base_data=$base_data --seg_len=1250 --toy --preprocess_files
     
     # Special case for mimic: also process with seg_len=2500
-    if [ "$base_data" = "mimic" ]; then
-        echo "Processing mimic with seg_len=2500"
-        python preprocess_ecg.py --base_data=$base_data --seg_len=2500 --toy --preprocess_files --sample_percentiles --sample_files --random_sampling # or --stratified_sampling
-    fi
+    # if [ "$base_data" = "mimic" ]; then
+    #     echo "Processing mimic with seg_len=2500"
+    #     python preprocess_ecg.py --base_data=$base_data --seg_len=2500 --toy --preprocess_files --sample_percentiles --sample_files --random_sampling # or --stratified_sampling
+    # fi
 done
 
 ### MAPPING DATA
