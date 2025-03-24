593376f87fb2b14834204f44802aece2d6a90509
willxxy <willhan327@outlook.com>
2025-03-24 14:12:36 +0000

    everything seems to work

--- ecg_bench/utils/conversation_utils.py
+++ ecg_bench/utils/conversation_utils.py
@@ -2073,25 +2073,33 @@ register_conv_template(
 if __name__ == "__main__":
     from transformers import AutoModelForCausalLM, AutoTokenizer
     # llama_model = "meta-llama/llama-3.2-1b-instruct"
-    llama_model = "meta-llama/llama-3.2-1b"
-    llama_tokenizer = AutoTokenizer.from_pretrained(llama_model, cache_dir='../.huggingface')
-    llama_model = AutoModelForCausalLM.from_pretrained(llama_model, cache_dir='../.huggingface')
-    
-    print("\n")
-    print("-- Llama-3 template --")
-    conv = get_conv_template("llama-3")
-    conv.set_system_message("You are a helpful assistant that can see and understand images. Please provide detailed and accurate responses.")
-    conv.append_message(conv.roles[0], "Hello!")
-    conv.append_message(conv.roles[1], "Hi!")
-    conv.append_message(conv.roles[0], "How are you?")
-    conv.append_message(conv.roles[1], "great")
-    print(conv.get_prompt())
-    print(conv.dict())
-    token_ids = llama_tokenizer.encode(conv.get_prompt())
-    tokens = llama_tokenizer.convert_ids_to_tokens(token_ids)
+    # llama_model = "meta-llama/llama-3.2-1b"
+    models = ["google/gemma-2-2b", "qwen/qwen2.5-1.5b"]
+    for checkpoint in models:
+        llama_tokenizer = AutoTokenizer.from_pretrained(checkpoint, cache_dir='../.huggingface')
+        llama_model = AutoModelForCausalLM.from_pretrained(checkpoint, cache_dir='../.huggingface')
+        
+        print("\n")
+        print(f"-- {checkpoint} template --")
+        # conv = get_conv_template("llama-3")
+        if 'gemma' in checkpoint:
+            conv = get_conv_template("gemma")
+        elif 'qwen' in checkpoint:
+            conv = get_conv_template("qwen-7b-chat")
+        conv.set_system_message("You are a helpful assistant that can see and understand images. Please provide detailed and accurate responses.")
+        conv.append_message(conv.roles[0], "Hello!")
+        conv.append_message(conv.roles[1], "Hi!")
+        conv.append_message(conv.roles[0], "How are you?")
+        conv.append_message(conv.roles[1], "great")
+        print(conv.get_prompt())
+        print(conv.dict())
+        token_ids = llama_tokenizer.encode(conv.get_prompt())
+        tokens = llama_tokenizer.convert_ids_to_tokens(token_ids)
 
-    print(llama_tokenizer.decode(token_ids))
-    
-    print("\nTokenized prompt with corresponding token IDs:")
-    for idx, (token, token_id) in enumerate(zip(tokens, token_ids)):
-        print(f"{idx}: {token} -> {token_id}")
+        print(llama_tokenizer.decode(token_ids))
+        
+        print("\nTokenized prompt with corresponding token IDs:")
+        for idx, (token, token_id) in enumerate(zip(tokens, token_ids)):
+            print(f"{idx}: {token} -> {token_id}")
+            
+        print('--' * 100)
