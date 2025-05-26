0e05e5888f8e5ef9ba47576cce35a3c16fef97f7
willxxy <willhan327@outlook.com>
2025-05-26 20:32:19 +0000

    preprocess test and good and more cleanups

--- ecg_bench/scripts/train_tokenizer.sh
+++ ecg_bench/scripts/train_tokenizer.sh
@@ -5,7 +5,6 @@ python train_tokenizer.py \
 --num_merges=5000 \
 --sampled_files=data/sampled_300000_random.txt \
 --num_cores=6 \
---percentiles=data/mimic_percentiles_2500_250_300000.npy \
 --train_tokenizer \
 --instance_normalize \
 --dev
@@ -15,7 +14,6 @@ python train_tokenizer.py \
 --num_merges=5000 \
 --sampled_files=data/sampled_300000_random.txt \
 --num_cores=6 \
---percentiles=data/mimic_percentiles_2500_250_300000.npy \
 --ecg_tokenizer=data/tokenizer_5000_300000_True_True.pkl \
 --instance_normalize \
 --dev
