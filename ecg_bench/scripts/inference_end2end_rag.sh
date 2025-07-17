a59ec05c373eb73bf3076d4d78bd7d104cc78b00
willxxy <willhan327@outlook.com>
2025-07-17 17:23:58 +0000

    update

--- ecg_bench/scripts/inference_end2end_rag.sh
+++ ecg_bench/scripts/inference_end2end_rag.sh
@@ -14,12 +14,12 @@
 python main.py \
 --data=ecg_instruct_45k_mapped_1250 \
 --model=llama-3.2-1b-instruct \
---device=cuda:0 \
+--device=cuda:6 \
 --ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
 --seg_len=1250 \
 --peft \
 --inference=end2end \
---checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_user_input_combined_combined_False \
+--checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_system_prompt_combined_combined_False \
 --system_prompt=./data/system_prompt_e2e.txt \
 --batch_size=1 \
 --epochs=1 \
@@ -33,12 +33,12 @@ python main.py \
 python main.py \
 --data=ecg_instruct_45k_mapped_1250 \
 --model=llama-3.2-1b-instruct \
---device=cuda:0 \
+--device=cuda:6 \
 --ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
 --seg_len=1250 \
 --peft \
 --inference=end2end \
---checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_user_input_combined_combined_False \
+--checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_system_prompt_combined_combined_False \
 --system_prompt=./data/system_prompt_e2e.txt \
 --batch_size=1 \
 --epochs=1 \
