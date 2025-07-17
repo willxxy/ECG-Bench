642220a08bcba2faa1239c6ad25bfed9db87b0d2
nbbb24 <sxysxysxysxy282828@gmail.com>
2025-07-17 15:52:46 +0000

    merge conflicts

--- ecg_bench/scripts/inference_end2end_rag.sh
+++ ecg_bench/scripts/inference_end2end_rag.sh
@@ -3,8 +3,8 @@
 # data=("ecg-qa_ptbxl_mapped_1250" "pretrain_mimic_mapped_1250" "ecg_instruct_45k_mapped_1250" "ecg_instruct_pulse_mapped_1250" "ecg-qa_mimic-iv-ecg_mapped_1250")
 # data=("ecg_instruct_45k_mapped_1250")
 
-<<<<<<< HEAD
-retrieved_information="combined"
+
+retrieval_base="signal"
 for d in "${data[@]}"; do
     if [ "$d" = "ecg_instruct_pulse_mapped_1250" ]; then
         data_arg="ecg_bench_pulse_mapped_1250"
@@ -27,51 +27,10 @@ for d in "${data[@]}"; do
     --instance_normalize \
     --attn_implementation=flash_attention_2 \
     --rag \
-    --retrieval_base=signal \
-    --retrieved_information=$retrieved_information \
+    --rag_k=1 \
+    --rag_prompt_mode=system_prompt \
+    --retrieval_base=$retrieval_base \
+    --retrieved_information=combined \
     --load_rag_db=./data/mimic/rag_metadata.json \
-    --load_rag_db_idx=./data/mimic/${retrieved_information}.index \
+    --load_rag_db_idx=./data/mimic/${retrieval_base}.index \
 done
-=======
-
-# for d in "${data[@]}"; do
-#     if [ "$d" = "ecg_instruct_pulse_mapped_1250" ]; then
-#         data_arg="ecg_bench_pulse_mapped_1250"
-#     else
-#         data_arg="$d"
-#     fi
-    
-python main.py \
---data=ecg_instruct_45k_mapped_1250 \
---model=llama-3.2-1b-instruct \
---device=cuda:0 \
---ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
---seg_len=1250 \
---peft \
---inference=end2end \
---checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_user_input_combined_combined_False \
---system_prompt=./data/system_prompt_e2e.txt \
---batch_size=1 \
---epochs=1 \
---instance_normalize \
---attn_implementation=flash_attention_2 \
---rag \
---load_rag_db=./data/mimic/rag_metadata.json \
---load_rag_db_idx=./data/mimic/combined.index
-# done
-
-python main.py \
---data=ecg_instruct_45k_mapped_1250 \
---model=llama-3.2-1b-instruct \
---device=cuda:0 \
---ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
---seg_len=1250 \
---peft \
---inference=end2end \
---checkpoint=./runs/ecg_instruct_45k_mapped_1250/0/llama-3.2-1b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_user_input_combined_combined_False \
---system_prompt=./data/system_prompt_e2e.txt \
---batch_size=1 \
---epochs=1 \
---instance_normalize \
---attn_implementation=flash_attention_2
->>>>>>> origin/main
--- ecg_bench/scripts/train_end2end_rag.sh
+++ ecg_bench/scripts/train_end2end_rag.sh
@@ -2,24 +2,29 @@
 
 # data=("ecg-qa_ptbxl_mapped_1250")
 
+retrieval_base="signal"
 
 # for d in "${data[@]}"; do
 python main.py \
---data=ecg_instruct_45k_mapped_1250 \
---model=llama-3.2-1b-instruct \
---device=cuda:1 \
---ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
---seg_len=1250 \
---peft \
---train=end2end \
---system_prompt=./data/system_prompt_e2e.txt \
---batch_size=2 \
---pad_to_max=1024 \
---epochs=1 \
---instance_normalize \
---attn_implementation=flash_attention_2 \
---rag \
---load_rag_db=./data/mimic/rag_metadata.json \
---load_rag_db_idx=./data/mimic/combined.index \
---log
+    --data=ecg_instruct_45k_mapped_1250 \
+    --model=llama-3.2-1b-instruct \
+    --device=cuda:1 \
+    --ecg_tokenizer=./data/tokenizer_5000_300000_instance.pkl \
+    --seg_len=1250 \
+    --peft \
+    --train=end2end \
+    --system_prompt=./data/system_prompt_e2e.txt \
+    --batch_size=2 \
+    --pad_to_max=1024 \
+    --epochs=1 \
+    --instance_normalize \
+    --attn_implementation=flash_attention_2 \
+    --rag \
+    --rag_k=1 \
+    --rag_prompt_mode=system_prompt \
+    --retrieval_base=$retrieval_base \
+    --retrieved_information=combined \
+    --load_rag_db=./data/mimic/rag_metadata.json \
+    --load_rag_db_idx=./data/mimic/${retrieval_base}.index \
+    --log
 # done
