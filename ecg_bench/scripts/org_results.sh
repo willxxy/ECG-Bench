a14c2b66b7fa4b6630235ff338bc7feb9b7b00a3
nbbb24 <sxysxysxysxy282828@gmail.com>
2025-09-13 03:22:12 +0000

    update

--- ecg_bench/scripts/org_results.sh
+++ ecg_bench/scripts/org_results.sh
@@ -11,7 +11,7 @@ data=("ecg-qa_ptbxl_mapped_1250")
 # rag_prompt_mode="system_prompt"
 # normalized_rag_features=True
 
-checkpoints='llama-3.2-3b-instruct_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_True_feature_report_1_system_prompt_None_False'
+checkpoints='siglip_llama-3.2-1b-instruct_adam_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_True_None_None_True_1_None_None_feature_report_1_system_prompt_None_False'
 # checkpoints='qwen2.5-3b_2_1_1024_0.0001_0.9_0.99_1e-08_500_0.01_True_None_None_None_None_False'
 
 for d in "${data[@]}"; do
@@ -23,6 +23,6 @@ for d in "${data[@]}"; do
     
     for ckpt in "${checkpoints[@]}"; do
         python organize_results.py \
-        --checkpoint=./runs/$d/0/$ckpt/ecgqa-mimic
+        --checkpoint=./runs/$d/0/$ckpt/
     done
 done
