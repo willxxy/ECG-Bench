2782a628ddb2c6b911b1d16acf41258713bbabce
willxxy <willhan327@outlook.com>
2025-10-18 22:27:37 +0000

    lets delete unused files

--- scripts/train_elm.sh
+++ scripts/train_elm.sh
@@ -1,11 +1,11 @@
 # single GPU (works as you had it)
 datasets=(
-  ecg-qa-mimic-iv-ecg-250-2500
-  ecg-qa-ptbxl-250-1250
-  pretrain-mimic-250-2500
-  ecg-instruct-45k-250-2500
-  ecg-bench-pulse-250-2500
-  ecg-instruct-pulse-250-2500
+  # ecg-qa-mimic-iv-ecg-250-1250
+  # ecg-qa-ptbxl-250-1250
+  # pretrain-mimic-250-1250
+  ecg-instruct-45k-250-1250
+  # ecg-bench-pulse-250-1250
+  # ecg-instruct-pulse-250-1250
 )
 for data in "${datasets[@]}"
 do
@@ -19,19 +19,19 @@ do
     --batch_size=2 \
     --attention_type=flash_attention_2 \
     --system_prompt=./ecg_bench/configs/system_prompt/system_prompt.txt \
-    --dev
+    --wandb
     echo "Finished training on $data"
     echo "-----------------------------------"
 done
 
-python -m ecg_bench.train_elm \
---ecg_image \
---batch_size=2 \
---llm=llama-3.2-1b-instruct \
---encoder=siglip-base-patch16-224 \
---data=ecg-qa-ptbxl-250-1250 \
---device=cuda:1 \
---peft \
---attention_type=flash_attention_2 \
---system_prompt=./ecg_bench/configs/system_prompt/system_prompt.txt \
---dev
+# python -m ecg_bench.train_elm \
+# --ecg_signal \
+# --batch_size=2 \
+# --llm=llama-3.2-1b-instruct \
+# --encoder=siglip-base-patch16-224 \
+# --data=ecg-instruct-45k-250-1250 \
+# --device=cuda:1 \
+# --peft \
+# --attention_type=flash_attention_2 \
+# --system_prompt=./ecg_bench/configs/system_prompt/system_prompt.txt \
+# --dev
