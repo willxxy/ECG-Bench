ac6b05e6cff28971821d54a0ae4e009512e2bb25
willxxy <willhan327@outlook.com>
2025-11-07 05:21:54 +0000

    update

--- scripts/train_elm.sh
+++ scripts/train_elm.sh
@@ -3,7 +3,7 @@ datasets=(
   # ecg-qa-mimic-iv-ecg-250-1250
   ecg-qa-ptbxl-250-1250
   # pretrain-mimic-250-1250
-  # ecg-instruct-45k-250-1250
+  ecg-instruct-45k-250-1250
   # ecg-bench-pulse-250-1250
   # ecg-instruct-pulse-250-1250
 )
@@ -15,10 +15,11 @@ do
     --data="$data" \
     --device=cuda:2 \
     --peft \
-    --encoder=projection \
+    --encoder=merl \
     --batch_size=2 \
     --attention_type=flash_attention_2 \
     --system_prompt=./ecg_bench/configs/system_prompt/system_prompt.txt \
+    --encoder_ckpt=./ecg_bench/runs/training/encoder/0/checkpoints/epoch_best.pt \
     --wandb
     echo "Finished training on $data"
     echo "-----------------------------------"
