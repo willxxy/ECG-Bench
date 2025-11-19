4081b87e25ec2e31b9e2e033841877dd72c728b8
willxxy <willhan327@outlook.com>
2025-11-19 03:19:38 +0000

    stop tracking

--- scripts/train_signal2vec.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python -m ecg_bench.train_signal2vec \
---sampled_file=./ecg_bench/configs/ecg_tokenizers/sampled_500000_random.txt \
---ecg_tokenizer=./ecg_bench/configs/ecg_tokenizers/ecg_byte_tokenizer_6000_800000.pkl
