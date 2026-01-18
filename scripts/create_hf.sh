f620b2508d812111af667a3e8ce4216f09028c89
willxxy <willhan327@outlook.com>
2026-01-18 03:16:44 +0000

    add hf

---
+++ scripts/create_hf.sh
@@ -0,0 +1,4 @@
+python create_folds.py \
+--input ecg_bench/data/pretrain_mimic/pretrain_mimic.json \
+--output ecg_bench/data/pretrain_mimic/pretrain_mimic_folds.json \
+--repo_id willxxy/pretrain-mimic-250-2500
